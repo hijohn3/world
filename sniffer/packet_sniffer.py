5e5c7597e2af8ae611b22482990a4dfd1eff3144
hijohn3 <130885222+hijohn3@users.noreply.github.com>
2023-12-19 23:13:31 +0900

    Update packet_sniffer.py

--- sniffer/packet_sniffer.py
+++ sniffer/packet_sniffer.py
@@ -1,5 +1,34 @@
 from socket import *
+import struct
 import os
+
+def parse_ipheader(data):
+    ipheader = struct.unpack('!BBHHHBBH4s4s', data[:20])
+    return ipheader
+
+def getDatagramSize(ipheader):
+    return str(ipheader[2])
+
+def getProtocol(ipheader):
+    protocols = {1:'ICMP', 6:'TCP', 17:'UDP'}
+    proto = ipheader[6]
+    if proto in protocols:
+        return protocols[proto]
+    else:
+        return 'OHTERS'
+    
+def getIP(ipheader):
+    src_ip = inet_ntoa(ipheader[8])
+    dest_ip = inet_ntoa(ipheader[9])
+    return (src_ip, dest_ip)
+
+def recvData(sock):
+    data = ''
+    try:
+        data = sock.recvfrom(65565)
+    except timeout:
+        data = ''
+    return data[0]
 
 def sniffing(host):
     if os.name == 'nt':
@@ -10,17 +39,28 @@ def sniffing(host):
     sniffer = socket(AF_INET, SOCK_RAW, sock_protocol)
     sniffer.bind((host, 0))
     sniffer.setsockopt(IPPROTO_IP, IP_HDRINCL, 1)
-    if os.name == 'nt':
-        sniffer.ioctl(SIO_RCVALL, RCVALL_ON)
-        packet = sniffer.recvfrom(65565)
-        print(packet)
-    
-    if os.name == 'nt':
-        sniffer.ioctl(SIO_RCVALL, RCVALL_OFF)
+
+    count = 1
+    try:
+        while True:
+            data = recvData(sniffer)
+            ipheader = parse_ipheader(data[:20])
+            datagramSize = getDatagramSize(ipheader)
+            protocol = getProtocol(ipheader)
+            src_ip, dest_ip = getIP(ipheader)
+            print(f'SNIFFED [{count}] -------------')
+            print(f'Datagram Size : {datagramSize}')
+            print(f'Protocol : {protocol}')
+            print(f'Source IP : {src_ip}')
+            print(f'Destination IP : {dest_ip}')
+            count += 1
+    except KeyboardInterrupt:
+        if os.name == 'nt':
+            sniffer.ioctl(SIO_RCVALL, RCVALL_OFF)
     
 def main():
-    #host = gethostbyname(gethostname())
-    host = '10.0.2.15'
+    host = gethostbyname(gethostname())
+    #host = '192.168.0.3'
     print(f'START SNIFFING at {host}')
     sniffing(host)
 
